464cdc451883d0308ae0ce57fd4e99443661e7af
Saad <saadshafiq259@gmail.com>
2021-04-04 11:39:38 +0500

    added eloquent commands

--- SupplyChain/routes/web.php
+++ SupplyChain/routes/web.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 use DB;
+use  App\Models\users;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,18 +35,64 @@ Route::get('/add-stock',AdminController::class .'@add_stock');
 
 Route::get('/add-rate',AdminController::class .'@add_rate');
 
-Route::get('/insert', function(){
+// Route::get('/insert', function(){
 
 
-    DB::insert('insert into users (name, email, password) values (?,?,?)', ['Saad','abc@gmail.com','12pass34']);
+//     DB::insert('insert into users (name, email, password) values (?,?,?)', ['Saad','abc2@gmail.com','12pass34']);
+
+
+// });
+
+// Route::get('/select', function(){
+
+
+//     $result=DB::select('select * from users where id= ?',[1]);
+//     return $result;
+
+
+// });
+
+// Route::get('/update',function(){
+
+//     $updated= DB::update('update users set name="Saad" where id=?',[1]);
+//     return $updated;
+
+
+// });
+
+// Route::get('/del',function(){
+
+//     $delt=DB::delete('delete from users where id=?',[1]);
+//     return $delt;
+
+// });
+
+// Route::get('/find', function(){
+
+//     $users = users::all();
+//     return $users;
+
+
+// });
+
+// Route::get('/find', function(){
+
+
+//     $users=users::find(3);
+//     return $users->name;
+
+
+// });
+
+Route::get('/findwhere',function(){
+
+
+    $users=users::where('id',3)->orderBy('id','desc')->take(1)->get();
+
+    return $users;
 
 
 });
-
-
-
-
-
 
 
 
