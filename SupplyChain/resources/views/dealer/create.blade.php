e5f5f6acbde3bffff6644fa4239335f8a4dc93de
Saad <saadshafiq259@gmail.com>
2021-04-07 11:16:43 +0500

    added laravel collective

--- SupplyChain/resources/views/dealer/create.blade.php
+++ SupplyChain/resources/views/dealer/create.blade.php
@@ -8,35 +8,81 @@
 <div class="card">
     <div class="card-body">
       <h4 class="card-title text-center" style="font-size:25px;">Add New Dealer</h4>
-      {{-- <p class="card-description"> Basic form elements </p> --}}
-      <form class="forms-sample row" enctype="multipart/form-data" method="POST"  action="/dealer"  >
 
-        {{ csrf_field() }}
+      {{-- <form class="forms-sample row" enctype="multipart/form-data" method="POST"  action="/dealer"  > --}}
+
+        {!! Form::open(['method'=>'POST', 'action'=>'DealerController@store' ,'class'=>'forms-sample row', ]) !!}
+
+
+        {{-- {{ csrf_field() }} --}}
         <div class="form-group col-md-6">
-          <label for="name">Facility Name</label>
+
+          {!! Form::label('name','Facility Name') !!}
+          {!! Form::text('name', null,['class'=>'form-control', 'placeholder' => 'e.g. Welcome Fruit Shop', 'required' => 'required'] ) !!}
+
+          {{-- <label for="name">Facility Name</label>
           <input type="text" class="form-control" required id="name" name="name" placeholder="e.g. Welcome Fruit Shop">
+        --}}
         </div>
 
+
+
         <div class="form-group col-md-6">
-            <label for="owner">Owner Name</label>
+
+
+          {!! Form::label('owner','Owner Name') !!}
+          {!! Form::text('owner', null,['class'=>'form-control', 'placeholder' => 'Owner Name', 'required' => 'required'] ) !!}
+
+
+            {{-- <label for="owner">Owner Name</label>
             <input type="text" class="form-control" required id="owner" name="owner" placeholder="Owner Name">
+          --}}
+         
           </div>
 
 
+
         <div class="form-group col-md-6">
-          <label for="cnic">CNIC</label>
-          <input type="text" class="form-control" required id="cnic" name="cnic" placeholder="CNIC">
+
+
+
+          {!! Form::label('cnic','CNIC') !!}
+          {!! Form::text('cnic', null,['class'=>'form-control', 'placeholder' => 'CNIC e.g. 3660123456791', 'required' => 'required'] ) !!}
+
+
+
+          {{-- <label for="cnic">CNIC</label>
+          <input type="text" class="form-control" required id="cnic" name="cnic" placeholder="CNIC e.g. 3660123456791">
+        --}}
+       
         </div>
 
         <div class="form-group col-md-6">
-            <label for="phone">Phone</label>
-            <input type="text" class="form-control" required id="phone" name="phone" placeholder="Phone">
+
+          
+          {!! Form::label('phone','Phone') !!}
+          {!! Form::text('phone', null,['class'=>'form-control', 'placeholder' => 'Phone e.g. 03012345678', 'required' => 'required'] ) !!}
+
+
+
+            {{-- <label for="phone">Phone</label>
+            <input type="text" class="form-control" required id="phone" name="phone" placeholder="Phone"> --}}
+
+
           </div>
 
 
           <div class="form-group col-md-6">
-            <label for="address">Address</label>
-            <input type="text" class="form-control" required id="address" name="address" placeholder="Address">
+
+            {!! Form::label('address','Address') !!}
+            {!! Form::text('address', null,['class'=>'form-control', 'placeholder' => 'Address', 'required' => 'required'] ) !!}
+  
+
+
+            {{-- <label for="address">Address</label>
+            <input type="text" class="form-control" required id="address" name="address" placeholder="Address"> --}}
+
+
           </div>
 
 
@@ -55,13 +101,27 @@
         </div> --}}
 
         <div class="form-group col-md-6">
-          <label for="longitude">Longitude</label>
-          <input type="text" class="form-control" id="longitude" name="longitude" placeholder="Longitude">
+
+            {!! Form::label('longitude','Longitude') !!}
+            {!! Form::text('longitude', null,['class'=>'form-control', 'placeholder' => 'longitude', 'required' => 'required'] ) !!}
+  
+
+
+
+          {{-- <label for="longitude">Longitude</label>
+          <input type="text" class="form-control" id="longitude" name="longitude" placeholder="Longitude"> --}}
+
+
         </div>
 
         <div class="form-group col-md-6">
-            <label for="latitude">Latitude</label>
-            <input type="text" class="form-control" id="latitude" name="latitude" placeholder="Latitude">
+          {!! Form::label('latitude','Latitude') !!}
+          {!! Form::text('latitude', null,['class'=>'form-control', 'placeholder' => 'Latitude', 'required' => 'required'] ) !!}
+
+
+
+            {{-- <label for="latitude">Latitude</label>
+            <input type="text" class="form-control" id="latitude" name="latitude" placeholder="Latitude"> --}}
           </div>
   
 
@@ -71,10 +131,18 @@
         </div> --}}
 
         <div class="form-group col-md-12">
-          <button type="submit" class="btn btn-success mr-2">Submit</button>
-          <button class="btn btn-light">Cancel</button>
+
+          {!! Form::submit('Add', ['class'=>'btn btn-success mr-2']) !!}
+
+
+          {{-- <button type="submit" class="btn btn-success mr-2">Submit</button> --}}
+          
         </div>
-      </form>
+
+
+      {{-- </form> --}}
+      {!! Form::close() !!}
+   
     </div>
   </div>
 
--- SupplyChain/routes/web.php
+++ SupplyChain/routes/web.php
@@ -261,4 +261,4 @@ Route::get('/add-rate',AdminController::class .'@add_rate');
 // });
 
 
-Route::resource('/dealer',DealerController::class);
+Route::resource('/dealer','DealerController');
