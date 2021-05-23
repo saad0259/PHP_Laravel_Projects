b6d6090b9778edbedff45622c1201fba90e0ebcc
Muhammad Saad Shafiq <saadshafiq259@gmail.com>
2021-05-23 19:28:15 +0500

    started REST API, modifie api.php

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/dealers', function(){
+
+    return "Hello from APi";
+
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
